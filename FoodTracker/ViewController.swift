aa9623643a0499e995956fcd80469d06ff35e59a
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-05-06 11:10:36 +0900

    Work with View Controllersまで

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Madogiwa on 2019/03/09.
+//  Copyright © 2019年 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,
+                      UITextFieldDelegate,
+                      UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate {
+  // MARK: Properties
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
+  @IBOutlet weak var photoImageView: UIImageView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Handle the text field’s user input through delegate callbacks.
+    nameTextField.delegate = self
+  }
+
+  //MARK: UITextFieldDelegate
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard.
+    textField.resignFirstResponder()
+    return true
+  }
+
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    mealNameLabel.text = textField.text
+  }
+
+  // MARK: UIImagePickerControllerDelegate
+  // MEMO: ユーザーが画像選択をキャンセルしたら呼ばれる
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    // 画像選択画面を閉じる.
+    dismiss(animated: true, completion: nil)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // 画像にはいろいろな種類があるので、使いたいオリジナル画像だけを取得
+    guard let selectedImage = info[.originalImage] as? UIImage else {
+      fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+    }
+    photoImageView.image = selectedImage
+    // 画像選択画面を閉じる.
+    dismiss(animated: true, completion: nil)
+  }
+
+  //MARK: Actions
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    nameTextField.text = "Default Text"
+  }
+
+  @IBAction func selectImageFromPhotoLibraly(_ sender: UITapGestureRecognizer) {
+    print("selectImageFromPhotoLibraly")
+    // キーボードの非表示
+    nameTextField.resignFirstResponder()
+    // UIImagePickerControllerを使って、ユーザーの画像選択周りの処理を行う。
+    let imagePickerController = UIImagePickerController()
+    // カメラを起動せず、保存されたライブラリからのみ取得するようにする。
+    imagePickerController.sourceType = .photoLibrary
+    // 画像選択時にimagePickerControllerを呼び出す
+    imagePickerController.delegate = self
+    present(imagePickerController, animated: true, completion: nil)
+  }
+}
